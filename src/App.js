504727c977e9dcce9ed9b89c9a42655209b14ce4
Janet Leon <leon.irenejanet@gmail.com>
2018-11-04 16:58:32 -0800

    created intro page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import MyName from './name-intro';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello
-      </div>
+      <section className="App">
+       <MyName/>
+      </section>
     );
   }
 }
---
+++ src/name-intro.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './name-intro.css';
+export function MyName (){
+    const webDev = '<webdev/>';
+    return (
+        <header>
+            <h1>Janet Leon</h1>
+            <h2>{webDev}</h2>
+        </header>
+    )
+}
+
+export default MyName;
