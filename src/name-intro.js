b031e43badea2394b6419cb6e1fdf7eb46bbeacf
Janet Leon <leon.irenejanet@gmail.com>
2018-11-10 00:49:31 -0800

    added favicon and deleted unnecessary files

--- src/name-intro.js
+++ src/name-intro.js
@@ -10,9 +10,16 @@ export function MyName (){
             <h1>Janet Leon</h1>
             <h2>{webDev}</h2>
            <div className='icons'>
+            <a href= 'https://www.linkedin.com/in/janetleon/' target='_blank' rel="noopener noreferrer" > 
             <i className="fab fa-linkedin-in"></i>
-            <i className="fab fa-github"></i>
+            </a>
+
+            <a href= 'https://github.com/jileon' target='_blank' rel="noopener noreferrer" > 
+            <i className="fab fa-github">
+            </i>
+            </a>
             </div>
+
         </header>
 
         
