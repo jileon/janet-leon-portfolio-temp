f319a4ee82dbabe3c1470f2c4e1d0e05eaa372b9
Janet Leon <leon.irenejanet@gmail.com>
2018-11-12 22:15:25 -0800

    Updates

---
+++ precache-manifest.133a90d2a04aaeb28273f911565b04f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa663b071fa77bde63fe031758413e9b",
+    "url": "/static/media/background.fa663b07.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ed750814cfa3be7a087",
+    "url": "/static/js/main.0ed75081.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "0ed750814cfa3be7a087",
+    "url": "/static/css/main.28e7f452.chunk.css"
+  },
+  {
+    "revision": "c9ffbd00fbca3435005508519391abf9",
+    "url": "/index.html"
+  }
+];
